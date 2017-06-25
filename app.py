889d0b8efd36aefe52e914fdf190cc6382af88c9
Navjot Singh <weavebytes@gmail.com>
2017-06-25 13:48:15 +0530

    ui (draft)

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import kivy
-
-
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import  ScreenManager, Screen
 
+# Builder.load_file("main.kv")
+Builder.load_file("second.kv")
+
+
+class MainScreen(Screen):
+    pass
+
+class SecondScreen(Screen):
+    pass
+
+sm = ScreenManager()
+# sm.add_widget(MainScreen(name = 'main'))
+sm.add_widget(SecondScreen(name = 'second'))
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return sm
 
 
 if __name__ == '__main__':
