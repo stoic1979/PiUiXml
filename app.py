54e2aa9d3f8e9c9313fd2bbd8ab23d77fd1b2cb9
Navjot Singh <weavebytes@gmail.com>
2017-06-27 18:45:43 +0530

    kv files

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
 # Config.set('graphics', 'fullscreen', 'fake')
 # Config.set('graphics', 'fullscreen', 1)
 
 
-# Builder.load_file("main.kv")
+Builder.load_file("main.kv")
+
 # Builder.load_file("second.kv")
-Builder.load_file("menu.kv")
+# Builder.load_file("menu.kv")
 
 
 class MainScreen(Screen):
@@ -22,10 +24,11 @@ class SecondScreen(Screen):
 class MenuScreen(Screen):
     pass
 
-sm = ScreenManager()
+
+# sm = ScreenManager()
 # sm.add_widget(MainScreen(name='main'))
 # sm.add_widget(SecondScreen(name='second'))
-sm.add_widget(MenuScreen(name='menu'))
+# sm.add_widget(MenuScreen(name='menu'))
 
 
 class MyApp(App):
@@ -33,7 +36,9 @@ class MyApp(App):
     data = ['A', 'B', 'C']
 
     def build(self):
-        return sm
+        return MainScreen()
 
 if __name__ == '__main__':
     MyApp().run()
+
+
