aac8c8ac91d1a34007811484433b8f2862e3e8dc
Navjot Singh <weavebytes@gmail.com>
2017-06-27 13:49:01 +0530

    tab panel test

---
+++ tests/tabpanel/tab_test.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.lang import Builder
+
+Builder.load_file("tabs.kv")
+
+
+
+class Test(TabbedPanel):
+    pass
+
+
+class TabbedPanelApp(App):
+    def build(self):
+        return Test()
+
+
+if __name__ == '__main__':
+    TabbedPanelApp().run()
