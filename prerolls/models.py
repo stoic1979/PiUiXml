0b91c57397625a616afae9f3386cebfc1b86fb74
Navjot Singh <weavebytes@gmail.com>
2017-06-27 19:37:32 +0530

    updated models

--- prerolls/models.py
+++ prerolls/models.py
@@ -11,6 +11,19 @@ class Pagination:
         self.count = count
 
 
+    def show_pagination_details(self):
+        print "\n================[ Product ]====================="
+        print "current_page . . . . . . . . . : ", self.current_page
+        print "last_page . . . . . . . . : ", self.last_page
+        print "per_page . . . . .: ", self.per_page
+        print "has_more_pages . . . . . . : ", self.has_more_pages
+        print "first_item . . . . . : ", self.first_item
+        print "last_item . . .  . .: ", self.last_item
+        print "total . . . . . : ", self.total
+        print "count . . .  . .: ", self.count
+        print "================================================\n"
+
+
 class Product:
     def __init__(self, id, name, dispensary_id, dispensary_name,
                  canabis_brand, canabis_strain, category, subcategory,
@@ -61,27 +74,18 @@ class Product:
         print "thc_levevaluel_type . . .  . .: ", self.thc_levevaluel_type
         print "cbd_level_type . . . . . : ", self.cbd_level_type
         print "cbn_level_type . . .  . .: ", self.cbn_level_type
-        print "prices . . .  . .: ", self.prices
-        print "urls . . .  . .: ", self.urls
-        print "images . . .  . .: ", self.images
+        print "prices . . .  . .: "
+        for price in self.prices:
+            print "  ", price
+        print "urls . . .  . .: ",
+        for url in self.urls:
+            print "    ", url
+        print "images . . .  . .: "
+        for image in self.images:
+            print "  ", image
         print "description . . .  . .: ", self.description
         print "created_at . . .  . .: ", self.created_at
         print "updated_at . . .  . .: ", self.updated_at
-
-
-        # for categories in self.category:
-        #     print categories
-        # print "units . . . . . .. . : "
-        # for unit in self.units:
-        #     print "  ", unit
-        # print "images . . . . . . . : "
-        # for image in self.images:
-        #     print "   ", image
-        # print "updated_at . . . . . : ", self.updated_at
-        # print "created_at . . .  . .: ", self.created_at
-        # print "logo . . . . . . . . : "
-        # for log in self.logo:
-        #     print "   ", log
         print "================================================\n"
 
 
