8409627b5021cf6d2410b05c57b8eae9bce8bf37
Navjot Singh <weavebytes@gmail.com>
2017-06-29 15:10:44 +0530

    adding extracts ui

---
+++ extracts/extracts.py
@@ -0,0 +1,32 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+# setting font
+from kivy.core.text import LabelBase
+LabelBase.register(name="GothamMedium",
+                   fn_regular="fonts/GothamMedium.ttf",
+                   fn_bold="fonts/GothamMedium.ttf",
+                   fn_italic="fonts/GothamMedium.ttf",
+                   fn_bolditalic="fonts/GothamMedium.ttf")
+
+
+Builder.load_file("extracts.kv")
+
+
+class MainScreen(Screen):
+    pass
+
+sm = ScreenManager()
+sm.add_widget(MainScreen(name='main'))
+
+
+class MyApp(App):
+
+    data = ['A', 'B', 'C']
+
+    def build(self):
+        return sm
+
+if __name__ == '__main__':
+    MyApp().run()
